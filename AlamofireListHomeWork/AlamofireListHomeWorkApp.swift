5c87dae825de7f4852b926f9429de95677da6c77
Nurali <nurali.shamel@icloud.com>
2024-04-27 00:36:59 +0500

    Initial Commit

---
+++ AlamofireListHomeWork/AlamofireListHomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireListHomeWorkApp.swift
+//  AlamofireListHomeWork
+//
+//  Created by Nurali Shamel on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireListHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
